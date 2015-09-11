c381dee4491b814b98a6cbb5c5094d19f3c9a94b
Brody Berson <brody.berson@me.com>
2015-09-11 17:36:15 -0600

    Main app to listen for Dash Button and Place your Default Order (payment not complete)

---
+++ app.js
@@ -0,0 +1,62 @@
+var dash_button = require('node-dash-button');
+var pizzapi = require('pizzapi');
+
+// Setup your Default Store
+// 6371 = Lafayette, CO 80026
+// Run `node findStore.js` and enter Zip Code
+var myStore = new pizzapi.Store(
+    {
+        ID: 6371
+    }
+);
+
+// Dummy Customer
+var thePresident = new pizzapi.Customer(
+    {
+        firstName: 'Barack',
+        lastName: 'Obama',
+        address: '700 Pennsylvania Avenue, Washington, DC',
+        email: 'barack@whitehouse.gov'
+    }
+);
+
+var order = new pizzapi.Order(
+    {
+        customer: thePresident,
+        storeID: myStore.ID
+    }
+);
+
+// Setup your Default Order
+// 14SCREEN = Large (14") Hand Tossed Pizza Whole: Cheese
+order.addItem(
+    new pizzapi.Item(
+        {
+            code: '14SCREEN',
+            options: {},
+            quantity: 1
+        }
+    )
+);
+
+var dash = dash_button("XX:3f:20:33:54:44"); //address from running `node node_modules/node-dash-button/bin/findbutton`
+dash.on("detected", function (){
+    console.log("Dash Button Found");
+	//Validate, price, and place order!
+	order.validate(
+	    function(result) {
+	        console.log("Order is Validated");
+	    }
+	);
+	order.price(
+	    function(result) {
+            console.log("Price is", result.result.Order.Amounts, "\nEstimated Wait Time",result.result.Order.EstimatedWaitMinutes, "minutes")
+	    }
+	);
+	//Still need to setup payment information
+	order.place(
+	    function(result) {
+	        console.log("Order placed!");
+	    }
+	);
+});
