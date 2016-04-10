a64f899d4d6ec40e7959e83e767a4d646f3c407d
Dan Gorman <danthemangorman@gmail.com>
2016-04-09 20:49:15 -0400

    added submit button to billing

--- website/flaskMain.py
+++ website/flaskMain.py
@@ -7,5 +7,14 @@ app=Flask(__name__)
 def home():
 	return render_template('index.html')
 
+@app.route('/customer')
+def customer():
+	return render_template('PersonalInformation.html')
+
+@app.route('/billing')
+def billing():
+	return render_template('CreditCardInfo.html')
+
+
 if __name__=="__main__":
 	app.run(debug=True);
